736feecefba6e168fed0f96031d9e28562a7f276
Mario Fink <mario.fink@goodyear.com>
2019-02-27 11:13:46 +0100

    use fetch to query API working

--- browserAction/script.js
+++ browserAction/script.js
@@ -1,1 +1,26 @@
-document.getElementById('myHeading').style.color = 'red'
+document.getElementById("myHeading").style.color = "red";
+
+const loginForm = document.getElementById("loginForm");
+const status = document.getElementById("status");
+
+const url = "https://api.pinboard.in/v1/user/api_token";
+const options = {
+  method: "GET",
+  mode: "cors",
+  headers: {
+    "Content-Type": "application/json"
+  }
+};
+
+loginForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const token = document.getElementById("apitoken").value;
+  fetch(url + `?auth_token=${token}`, options)
+    .then(response => {
+      console.log("RESPONSE", response);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  status.innerHTML = "Submitted";
+});
