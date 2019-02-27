a843ae00213cf1a10a1e2850b1b58398716b6c52
Mario Fink <mario.fink@goodyear.com>
2019-02-27 13:58:12 +0100

    add apikey to options page

--- background_script.js
+++ background_script.js
@@ -1,1 +1,15 @@
 // Put all the javascript code here, that you want to execute in background.
+
+function loadOptions(options) {
+  const apikey = options.apikey;
+  if (typeof apikey !== "undefined") {
+    console.log("loaded apikey", apikey);
+  } else {
+    console.log("no apikey found");
+  }
+}
+
+// Fires on startup
+browser.storage.local.get().then(loadOptions, err => {
+  console.error("error local storage");
+});
--- options/script.js
+++ options/script.js
@@ -1,1 +1,17 @@
-document.getElementById('myHeading').style.color = 'red'
+const apiTokenInput = document.querySelector("#apitoken");
+
+function saveOptions(e) {
+  e.preventDefault();
+  browser.storage.sync.set({
+    apitoken: apiTokenInput.value
+  });
+}
+
+function restoreOptions() {
+  browser.storage.sync.get("apitoken").then(res => {
+    apiTokenInput.value = res.apitoken || "";
+  });
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
