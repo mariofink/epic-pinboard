51f1d05a388690ff6d9f874a69209e04acd155d0
Mario Fink <mario.fink@goodyear.com>
2019-03-18 08:45:15 +0100

    if nothing is selected, try to get description from meta tag

--- src/content_script.js
+++ src/content_script.js
@@ -1,6 +1,15 @@
 browser.runtime.onMessage.addListener(request => {
   if (request.action === "GET_DESCRIPTION") {
-    let selection = window.getSelection().toString();
-    return Promise.resolve(selection);
+    let description = window.getSelection().toString();
+    if (description.length < 1) {
+      // if nothing is selected, try to get description from meta tag
+      const metaDescription = document.querySelector(
+        "meta[name='description']"
+      );
+      if (metaDescription) {
+        description = metaDescription.getAttribute("content");
+      }
+    }
+    return Promise.resolve(description);
   }
 });
