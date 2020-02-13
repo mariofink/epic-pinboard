055691cafa3e443e22af0720c82d382cf26610ca
Mario Fink <mario.fink@goodyear.com>
2020-02-13 22:57:13 +0100

    add vue component

---
+++ src/components/AddBookmark/AddBookmark.vue
@@ -0,0 +1,208 @@
+<template>
+  <div>
+    <div v-if="token.length < 1">
+      <div class="panel-section panel-section-header">
+        <p>Please provide your Pinboard API token in the options.</p>
+      </div>
+      <div class="panel-section panel-section-footer">
+        <div
+          class="panel-section-footer-button default"
+          @click="openOptions()"
+        >Open Epic Pinboard options</div>
+      </div>
+    </div>
+    <div v-else>
+      <div v-show="loading">
+        <div class="ball-pulse">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+      <div class="panel-section panel-section-formElements" v-show="!loading">
+        <form id="addBoomarkForm">
+          <div class="form-item browser-style">
+            <label for="url">URL</label>
+            <input type="text" name="url" id="url" maxlength="2000" v-model="url">
+          </div>
+          <div class="form-item browser-style">
+            <label for="title">Title</label>
+            <input type="text" name="title" id="title" maxlength="255" v-model="title">
+          </div>
+          <div class="form-item browser-style">
+            <label for="tags">Tags</label>
+            <vue-tags-input
+              v-model="tag"
+              :tags="tags"
+              :autocomplete-items="filteredTags"
+              @tags-changed="newTags => tags = newTags"
+            />
+          </div>
+          <div class="form-item browser-style">
+            <label for="suggested">Suggested Tags</label>
+            <ul class="suggested-tags">
+              <li
+                class="suggested-tag"
+                v-for="tag in suggestedTags"
+                :key="tag"
+                @click="addSuggestedTag(tag)"
+              >{{tag}}</li>
+            </ul>
+          </div>
+          <div class="form-item browser-style">
+            <label for="notes">Notes</label>
+            <textarea
+              class="browser-style"
+              name="notes"
+              id="notes"
+              maxlength="65536"
+              v-model="notes"
+            ></textarea>
+          </div>
+          <div class="form-item browser-style">
+            <input type="checkbox" id="private" name="private" v-model="privateBookmark">
+            <label for="private" class="inline">Private bookmark</label>
+          </div>
+          <div class="form-item browser-style">
+            <input type="checkbox" id="readlater" name="readlater" v-model="readLater">
+            <label for="readlater" class="inline">Read later</label>
+          </div>
+        </form>
+      </div>
+      <div class="panel-section panel-section-footer" v-show="!loading">
+        <div
+          class="panel-section-footer-button default"
+          id="addBookmarkButton"
+          @click="addBookmark"
+        >{{ buttonCaption }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { VueTagsInput, createTags } from "@johmun/vue-tags-input";
+
+function getActiveTab() {
+  return new Promise((resolve, reject) => {
+    browser.tabs.query({ active: true, currentWindow: true }, tabs => {
+      resolve(tabs[0])
+    });
+  });
+}
+
+const ctaAddBookmark = "Add bookmark";
+const ctaUpdateBookmark = "Update bookmark";
+
+export default {
+  components: {
+    VueTagsInput
+  },
+  data() {
+    return {
+      token: "",
+      allTags: [],
+      url: "",
+      title: "",
+      suggestedTags: [],
+      notes: "",
+      privateBookmark: false,
+      tags: [],
+      tag: "",
+      readLater: false,
+      loading: true,
+      buttonCaption: ctaAddBookmark
+    };
+  },
+  async mounted() {
+    const background = await browser.runtime.getBackgroundPage();
+    this.token = await background.retrieveApiToken();
+    const allTagsObject = await background.getAllTags();
+    this.allTags = allTagsObject.map(i => i.name);
+    getActiveTab().then(tab => {
+      this.url = tab.url;
+      this.title = tab.title;
+      background.getBookmarksForUrl(tab.url).then(bookmarks => {
+        if (bookmarks.posts.length > 0) {
+          this.buttonCaption = ctaUpdateBookmark;
+          const existingBookmark = bookmarks.posts[0];
+          this.title = existingBookmark.description;
+          this.notes = existingBookmark.extended;
+          this.privateBookmark = existingBookmark.shared === "no";
+          this.readLater = existingBookmark.toread === "yes";
+          if (existingBookmark.tags.length > 0) {
+            const existingTags = existingBookmark.tags.split(" ");
+            this.tags = existingTags;
+            if (existingTags.length > 0) {
+              this.tags = createTags(existingTags);
+            }
+          }
+        } else {
+          browser.tabs
+            .sendMessage(tab.id, {
+              action: "GET_DESCRIPTION"
+            })
+            .then(
+              description => {
+                this.notes = description;
+              },
+              err => {
+                console.error("err", err);
+              }
+            );
+        }
+      });
+      background
+        .getSuggestedTagsForUrl(tab.url)
+        .then(suggestions => {
+          this.suggestedTags = suggestions[1].recommended;
+          this.loading = false;
+        })
+        .catch(err => {
+          this.loading = false;
+        });
+    });
+  },
+  methods: {
+    openOptions() {
+      browser.runtime.openOptionsPage();
+      window.close();
+    },
+    addSuggestedTag(tag) {
+      this.tags.push({ text: tag });
+    },
+    async addBookmark() {
+      const background = await browser.runtime.getBackgroundPage();
+      const bookmark = {
+        url: this.url,
+        title: this.title,
+        tags: this.tags.map(tag => tag.text).join(" "),
+        notes: this.notes,
+        shared: this.privateBookmark ? "no" : "yes",
+        toread: this.readLater ? "yes" : "no"
+      };
+      background.addBookmark(bookmark).then(response => {
+        getActiveTab().then(tab => {
+          background.setActiveIcon(true, tab.id);
+        })
+        window.close();
+      });
+    }
+  },
+  computed: {
+    filteredTags: function() {
+      const filtered = this.allTags
+        .filter(tag => {
+          return tag.toLowerCase().startsWith(this.tag.toLowerCase());
+        })
+        .map(tag => {
+          return {
+            text: tag
+          };
+        });
+      return filtered.slice(0, 5);
+    }
+  }
+};
+</script>
+
--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,30 +1,11 @@
-import browser from 'webextension-polyfill';
+import Vue from "vue";
+import AddBookmark from "../components/AddBookmark/AddBookmark.vue";
 
-function openWebPage(url) {
-    return browser.tabs.create({ url });
-}
-
-document.addEventListener('DOMContentLoaded', async () => {
-    const tabs = await browser.tabs.query({
-        active: true,
-        lastFocusedWindow: true,
-    });
-
-    const url = tabs.length && tabs[0].url;
-
-    const response = await browser.runtime.sendMessage({
-        msg: 'hello',
-        url,
-    });
-
-    // eslint-disable-next-line no-console
-    console.log(response);
-
-    document.getElementById('github__button').addEventListener('click', () => {
-        return openWebPage('https://github.com/abhijithvijayan/web-extension-starter');
-    });
-
-    document.getElementById('donate__button').addEventListener('click', () => {
-        return openWebPage('https://www.buymeacoffee.com/abhijithvijayan');
-    });
+console.log("ADD", AddBookmark);
+new Vue({
+  el: "#pinboardAddBookmark",
+  render(h) {
+    return h("AddBookmark");
+  },
+  components: { AddBookmark },
 });
