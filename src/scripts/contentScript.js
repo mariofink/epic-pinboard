5dac1f0fb291224f0eb88e5851f23831e40b9280
Mario Fink <mario.fink@goodyear.com>
2020-02-20 22:48:58 +0100

    add GET_DESCRIPTION message handler

--- src/scripts/contentScript.js
+++ src/scripts/contentScript.js
@@ -1,1 +1,15 @@
-// document.body.style.filter = 'invert(1)';
+browser.runtime.onMessage.addListener(request => {
+  if (request.action === "GET_DESCRIPTION") {
+    let description = window.getSelection().toString();
+    if (description.length < 1) {
+      // if nothing is selected, try to get description from meta tag
+      const metaDescription = document.querySelector(
+        "meta[name='description']"
+      );
+      if (metaDescription) {
+        description = metaDescription.getAttribute("content");
+      }
+    }
+    return Promise.resolve(description);
+  }
+});
