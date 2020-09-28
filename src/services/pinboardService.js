3edd22e59acf9878fb4bc9b5088be2c2f180ae4b
Mario Fink <mario.fink@goodyear.com>
2020-09-28 21:36:48 +0200

    add urlcat and replace simple string urls in service class

--- src/services/pinboardService.js
+++ src/services/pinboardService.js
@@ -1,11 +1,15 @@
+import urlcat from "urlcat";
+
 export default class PinboardService {
   constructor(baseApiUrl) {
     this.baseApiUrl = baseApiUrl;
   }
   login(token) {
-    const url = this.baseApiUrl + "/user/api_token";
+    const url = urlcat(this.baseApiUrl, "/user/api_token", {
+      auth_token: token,
+    });
     return new Promise((resolve, reject) => {
-      fetch(url + `?auth_token=${token}`)
+      fetch(url)
         .then((response) => {
           resolve(response);
         })
@@ -16,9 +20,13 @@ export default class PinboardService {
   }
 
   getSuggestedTagsForUrl(token, bookmarkUrl) {
-    const url = this.baseApiUrl + "/posts/suggest";
+    const url = urlcat(this.baseApiUrl, "/posts/suggest", {
+      auth_token: token,
+      url: bookmarkUrl,
+      format: "json",
+    });
     return new Promise((resolve, reject) => {
-      fetch(url + `?auth_token=${token}&url=${bookmarkUrl}&format=json`)
+      fetch(url)
         .then((response) => {
           resolve(response.json());
         })
@@ -29,9 +37,13 @@ export default class PinboardService {
   }
 
   getBookmarksForUrl(token, bookmarkUrl) {
-    const url = this.baseApiUrl + "/posts/get";
+    const url = urlcat(this.baseApiUrl, "/posts/get", {
+      auth_token: token,
+      url: bookmarkUrl,
+      format: "json",
+    });
     return new Promise((resolve, reject) => {
-      fetch(url + `?auth_token=${token}&url=${bookmarkUrl}&format=json`)
+      fetch(url)
         .then((response) => {
           resolve(response.json());
         })
@@ -42,9 +54,12 @@ export default class PinboardService {
   }
 
   getAllTags(token) {
-    const url = this.baseApiUrl + "/tags/get";
+    const url = urlcat(this.baseApiUrl, "/tags/get", {
+      auth_token: token,
+      format: "json",
+    });
     return new Promise((resolve, reject) => {
-      fetch(url + `?auth_token=${token}&format=json`)
+      fetch(url)
         .then((response) => {
           resolve(response.json());
         })
@@ -55,12 +70,17 @@ export default class PinboardService {
   }
 
   addBookmark(token, bookmark) {
-    const url = this.baseApiUrl + "/posts/add";
+    const url = urlcat(this.baseApiUrl, "/posts/add", {
+      auth_token: token,
+      url: bookmark.url,
+      description: bookmark.title,
+      extended: bookmark.notes,
+      tags: bookmark.tags,
+      shared: bookmark.shared,
+      toread: bookmark.toread,
+    });
     return new Promise((resolve, reject) => {
-      fetch(
-        url +
-          `?auth_token=${token}&url=${bookmark.url}&description=${bookmark.title}&extended=${bookmark.notes}&tags=${bookmark.tags}&shared=${bookmark.shared}&toread=${bookmark.toread}`
-      )
+      fetch(url)
         .then((response) => {
           resolve(response);
         })
